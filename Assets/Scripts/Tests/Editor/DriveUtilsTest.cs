72b91b789c069e8691a6aecdfa0aaea1799859dd
Lucas Marguet <lucas.marguet@wooga.net>
2018-05-22 22:24:53 +0200

    refactored angled direction calculation test

--- Assets/Scripts/Tests/Editor/DriveUtilsTest.cs
+++ Assets/Scripts/Tests/Editor/DriveUtilsTest.cs
@@ -4,44 +4,41 @@ using UnityEngine;
 
 namespace Tests.Editor
 {
-	public class DriveUtilsTest {
+    public class DriveUtilsTest
+    {
+        [Test]
+        public void TestHitDistance()
+        {
+            Assert.AreEqual(DriveUtils.NormalizeHitDistance(200, 200), 0.5f);
+            Assert.AreEqual(DriveUtils.NormalizeHitDistance(200, 100), 0.5f);
+            Assert.AreEqual(DriveUtils.NormalizeHitDistance(200, 0), 1);
+            Assert.AreEqual(DriveUtils.NormalizeHitDistance(200, 50), 1);
+            Assert.AreEqual(DriveUtils.NormalizeHitDistance(200, 150), 0.5f);
+        }
 
-		[Test]
-		public void TestHitDistance() {
-			Assert.AreEqual(DriveUtils.NormalizeHitDistance(200, 200), 0.5f);
-			Assert.AreEqual(DriveUtils.NormalizeHitDistance(200, 100), 0.5f);
-			Assert.AreEqual(DriveUtils.NormalizeHitDistance(200, 0), 1);
-			Assert.AreEqual(DriveUtils.NormalizeHitDistance(200, 50), 1);
-			Assert.AreEqual(DriveUtils.NormalizeHitDistance(200, 150), 0.5f);
-		}
+        [Test]
+        public void TestAngleDirection()
+        {
+            AssetAngleDirectionCalculation(0, Vector3.left, new Vector3(-1, 0, 0), 0);
+            AssetAngleDirectionCalculation(0, Vector3.right, new Vector3(1, 0, 0), 0);
+            AssetAngleDirectionCalculation(0, Vector3.forward, new Vector3(0, 0, 1), 0);
 
-		[Test]
-		public void TestAngleDirection()
-		{
-			AssertVectorEquality(DriveUtils.CalculateAngleDirection(0, Vector3.left), new Vector3(-1, 0, 0), 0);
-			AssertVectorEquality(DriveUtils.CalculateAngleDirection(0, Vector3.right), new Vector3(1, 0, 0), 0);
-			AssertVectorEquality(DriveUtils.CalculateAngleDirection(0, Vector3.forward),  new Vector3(0, 0, 1), 0);
-			
-			AssertVectorEquality(DriveUtils.CalculateAngleDirection(45, Vector3.right),  new Vector3(0.7f, 0, -0.7f), 0.05f);
-			AssertVectorEquality(DriveUtils.CalculateAngleDirection(-45, Vector3.right),  new Vector3(0.7f, 0, 0.7f), 0.05f);
-			
-			AssertVectorEquality(DriveUtils.CalculateAngleDirection(90, -Vector3.right),  new Vector3(0, 0, 1), 0.1f);
-		}
-		
-		private static void AssertVectorEquality(Vector3 vectorA, Vector3 vectorB, float tolerance)
-		{
-			Assert.IsTrue(
-				MathUtils.AreVectorEquals(vectorA, vectorB, tolerance)
-			);
-		}
+            AssetAngleDirectionCalculation(45, Vector3.right, new Vector3(0.7f, 0, -0.7f), 0.05f);
+            AssetAngleDirectionCalculation(-45, Vector3.right, new Vector3(0.7f, 0, 0.7f), 0.05f);
 
-		// A UnityTest behaves like a coroutine in PlayMode
-		// and allows you to yield null to skip a frame in EditMode
-//	[UnityTest]
-//	public IEnumerator NewEditModeTestWithEnumeratorPasses() {
-//		// Use the Assert class to test conditions.
-//		// yield to skip a frame
-//		yield return null;
-//	}
-	}
+            AssetAngleDirectionCalculation(90, -Vector3.right, new Vector3(0, 0, 1), 0.1f);
+        }
+
+        private void AssetAngleDirectionCalculation(float angle, Vector3 direction, Vector3 compare, float tolerance)
+        {
+            AssertVectorEquality(DriveUtils.CalculateAngleDirection(angle, direction), compare, tolerance);
+        }
+
+        private static void AssertVectorEquality(Vector3 vectorA, Vector3 vectorB, float tolerance)
+        {
+            Assert.IsTrue(
+                MathUtils.AreVectorEquals(vectorA, vectorB, tolerance)
+            );
+        }
+    }
 }
